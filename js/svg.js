4bd61da9fcfc979d1570fa9a5673a3f88fbb9298
dargi <19378379+dargi@users.noreply.github.com>
2020-06-26 18:08:22 +0200

    :bug: Fix BUG loop animation

--- js/svg.js
+++ js/svg.js
@@ -3,6 +3,11 @@ var svgContainer = document.getElementById('svgContainer');
 var animItem = bodymovin.loadAnimation({
   wrapper: svgContainer,
   animType: 'svg',
-  loop: true,
+  loop: false,
   path: '../elumni_comingsoon.json'
+});
+
+animItem.addEventListener('DOMLoaded', function(){
+  animItem.setSpeed(0.7);
+  animItem.playSegments( [1,55], true );
 });
